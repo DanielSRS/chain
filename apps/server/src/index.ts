e4dfc1f2bc0f7a0f5fd68c19adeec1e04ff3b527
Daniel Santa Rosa <Daniel_srs_@hotmail.com>
2025-05-20 05:13:36 -0300

    refactor: move mqtt server to its own file

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,65 +1,11 @@
 import './server';
-import { Elysia, t } from 'elysia';
-import Paho from 'paho-mqtt';
-import { Logger } from './utils/logger.ts';
-import { interpret } from 'xstate';
 import { paxos } from './utils/paxos.ts';
+import { Logger } from './utils/logger.ts';
+import { Elysia, t } from 'elysia';
+import { interpret } from 'xstate';
 
 const log = Logger.extend('Server');
-
-const mqttHost = process.env.MQTT_HOST || 'localhost';
-const mqttPort = parseInt(process.env.MQTT_PORT || '9001');
-const wsPath = process.env.MQTT_PATH || '/';
 const SERVER_PORT = parseInt(process.env.SERVER_PORT || '3000');
-
-log.info(
-  `Connecting to MQTT broker at ${mqttHost}:${mqttPort} with path ${wsPath}`,
-);
-
-const mqttClient = new Paho.Client(
-  mqttHost,
-  mqttPort,
-  wsPath,
-  Date.now().toString() + 'Server',
-);
-
-mqttClient.onConnectionLost = responseObject => {
-  if (responseObject.errorCode !== 0) {
-    log.warn('MQTT connection lost: ' + responseObject.errorMessage);
-  }
-};
-
-mqttClient.onMessageArrived = message => {
-  log.info(
-    'Message arrived at topic: ',
-    message.destinationName,
-    message.payloadString,
-  );
-};
-
-mqttClient.connect({
-  onSuccess: () => {
-    log.info('Connected to MQTT broker successfully');
-    mqttClient.subscribe('test/topic', {
-      onSuccess: () => {
-        log.info('Subscribed to topic');
-
-        const message = new Paho.Message('Hello from server');
-        message.destinationName = 'test/topic';
-        mqttClient.send(message);
-      },
-      onFailure: err => {
-        log.error('Failed to subscribe to topic', err);
-      },
-    });
-  },
-  onFailure: error => {
-    log.error('Failed to connect to MQTT broker', error);
-  },
-  useSSL: false,
-  reconnect: true,
-  timeout: 10,
-});
 
 const app = new Elysia()
   .decorate('machine', interpret(paxos).start())
---
+++ apps/server/src/mqtt-server.ts
@@ -0,0 +1,57 @@
+import Paho from 'paho-mqtt';
+import { Logger } from './utils/logger.ts';
+
+const mqttHost = process.env.MQTT_HOST || 'localhost';
+const mqttPort = parseInt(process.env.MQTT_PORT || '9001');
+const wsPath = process.env.MQTT_PATH || '/';
+
+const log = Logger.extend('MQTT-Server');
+
+log.info(
+  `Connecting to MQTT broker at ${mqttHost}:${mqttPort} with path ${wsPath}`,
+);
+
+const mqttClient = new Paho.Client(
+  mqttHost,
+  mqttPort,
+  wsPath,
+  Date.now().toString() + 'Server',
+);
+
+mqttClient.onConnectionLost = responseObject => {
+  if (responseObject.errorCode !== 0) {
+    log.warn('MQTT connection lost: ' + responseObject.errorMessage);
+  }
+};
+
+mqttClient.onMessageArrived = message => {
+  log.info(
+    'Message arrived at topic: ',
+    message.destinationName,
+    message.payloadString,
+  );
+};
+
+mqttClient.connect({
+  onSuccess: () => {
+    log.info('Connected to MQTT broker successfully');
+    mqttClient.subscribe('test/topic', {
+      onSuccess: () => {
+        log.info('Subscribed to topic');
+
+        const message = new Paho.Message('Hello from server');
+        message.destinationName = 'test/topic';
+        mqttClient.send(message);
+      },
+      onFailure: err => {
+        log.error('Failed to subscribe to topic', err);
+      },
+    });
+  },
+  onFailure: error => {
+    log.error('Failed to connect to MQTT broker', error);
+  },
+  useSSL: false,
+  reconnect: true,
+  timeout: 10,
+});
